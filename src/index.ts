89b72dee4d4df2787448ccc2f30ac31232ffc363
COOL <yigitgurses2012@gmail.com>
2024-05-16 23:25:28 +0300

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,42 @@
-export function sayHello(name: string) {
-    console.log(`Hello, ${name}!!`)
+export const isEnvBrowser = (): boolean => !(window as any).invokeNative
+
+export const fetchNui = async (name: string, data?: object) => {
+    const options = {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8'
+        },
+        body: JSON.stringify(data)
+    }
+
+    const resourceName = (window as any).GetParentResourceName
+        ? (window as any).GetParentResourceName()
+        : 'nui-frame-app'
+
+    const response = await fetch(`https://${resourceName}/${name}`, options)
+
+    const formattedResponse = await response.json()
+
+    return formattedResponse
+}
+
+export const useNuiEvent = (
+    handler: (event: { action: string; data?: object }) => void
+) => {
+    const eventListener = ({ data }: MessageEvent) => handler(data)
+
+    window.onload = () => window.addEventListener('message', eventListener)
+    window.onunload = () => window.removeEventListener('message', eventListener)
+}
+
+export const sendDebugEvent = (name: string, data?: object) => {
+    if (isEnvBrowser())
+        window.dispatchEvent(
+            new MessageEvent('message', {
+                data: {
+                    action: name,
+                    data: data
+                }
+            })
+        )
 }
